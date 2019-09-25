11828e7e91610a430343c78b271a1482b3bfb065
GreenhornDeveloper <GreenhornDeveloper@users.noreply.github.com>
2019-09-25 11:31:59 -0400

    Update Factory.ts

--- Factory.ts
+++ Factory.ts
@@ -1,94 +1,90 @@
 //tsc Factory.ts && node Factory.js
 
 class Car{
+    make: string;
     model: string;
     passengerCount: number;
     driveTrain: string
 }
 
 abstract class AutomobileFactory{
-    make: string;
+    car: Car;
 
-    constructor(make: string){
-        this.make = make;
-    }
+    constructor(){}
 
    abstract buildCar(type: Car) : void;
 
 }
 
-
-class ToyotaFactory extends AutomobileFactory{
+class AutomobileCreator extends AutomobileFactory{
     
     buildCar(type: Car){
-        console.log(`${type.passengerCount} passenger ${this.make} ${type.model} with a ${type.driveTrain} engine was just made!`)
+        if(type.make == 'Toyota'){
+            return new ToyotaFactory().buildCar(type)
+        } else if (type.make == 'Honda'){
+            return new HondaFactory().buildCar(type)
+        } else{
+            return 'Not found'
+        }
+        
     }
 
 }
 
-class HondaFactory extends AutomobileFactory{
+class ToyotaFactory{
+    
+    buildCar(type: Car){
+        console.log(`${type.passengerCount} passenger ${type.make} ${type.model} with a ${type.driveTrain} engine was just made!`)
+    }
+
+}
+
+class HondaFactory{
 
     buildCar(type: Car){
-        console.log(`You just built a ${type.passengerCount} passenger ${this.make} ${type.model} with a ${type.driveTrain} engine.`)
+        console.log(`You just built a ${type.passengerCount} passenger ${type.make} ${type.model} with a ${type.driveTrain} engine.`)
     }
 }
 
-const toyotaFactory = new ToyotaFactory('Toyota')
-toyotaFactory.buildCar({model:'Sienna', passengerCount: 8, driveTrain:'V6'})
-
-const hondaFactory = new HondaFactory('Honda')
-hondaFactory.buildCar({model:'Odyssey', passengerCount: 8, driveTrain:'V6'})
-
-
-
-
+const toyotaMinivan = new AutomobileCreator().buildCar({make: 'Toyota', model:'Sienna', passengerCount: 8, driveTrain:'V6'})
+const hondaMinivan = new AutomobileCreator().buildCar({make: 'Honda', model:'Odyssey', passengerCount: 8, driveTrain:'V6'})
 
 
 // class Car{
-//     make: string;
 //     model: string;
 //     passengerCount: number;
 //     driveTrain: string
 // }
 
 // abstract class AutomobileFactory{
-//     car: Car;
+//     make: string;
 
-//     constructor(){}
+//     constructor(make: string){
+//         this.make = make;
+//     }
 
 //    abstract buildCar(type: Car) : void;
 
 // }
 
-// class AutomobileCreator extends AutomobileFactory{
+
+// class ToyotaFactory extends AutomobileFactory{
     
 //     buildCar(type: Car){
-//         if(type.make == 'Toyota'){
-//             return new ToyotaFactory().buildCar(type)
-//         } else if (type.make == 'Honda'){
-//             return new HondaFactory().buildCar(type)
-//         } else{
-//             return 'Not found'
-//         }
-        
+//         console.log(`${type.passengerCount} passenger ${this.make} ${type.model} with a ${type.driveTrain} engine was just made!`)
 //     }
 
 // }
 
-// class ToyotaFactory{
-    
-//     buildCar(type: Car){
-//         console.log(`${type.passengerCount} passenger ${type.make} ${type.model} with a ${type.driveTrain} engine was just made!`)
-//     }
-
-// }
-
-// class HondaFactory{
+// class HondaFactory extends AutomobileFactory{
 
 //     buildCar(type: Car){
-//         console.log(`You just built a ${type.passengerCount} passenger ${type.make} ${type.model} with a ${type.driveTrain} engine.`)
+//         console.log(`You just built a ${type.passengerCount} passenger ${this.make} ${type.model} with a ${type.driveTrain} engine.`)
 //     }
 // }
 
-// const toyotaMinivan = new AutomobileCreator().buildCar({make: 'Toyota', model:'Sienna', passengerCount: 8, driveTrain:'V6'})
-// const hondaMinivan = new AutomobileCreator().buildCar({make: 'Honda', model:'Odyssey', passengerCount: 8, driveTrain:'V6'})
+// const toyotaFactory = new ToyotaFactory('Toyota')
+// toyotaFactory.buildCar({model:'Sienna', passengerCount: 8, driveTrain:'V6'})
+
+// const hondaFactory = new HondaFactory('Honda')
+// hondaFactory.buildCar({model:'Odyssey', passengerCount: 8, driveTrain:'V6'})
