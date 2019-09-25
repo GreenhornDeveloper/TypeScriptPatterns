277198f52fe77ed6521c266dee80690d52b014cf
GreenhornDeveloper <GreenhornDeveloper@users.noreply.github.com>
2019-09-25 11:19:48 -0400

    Update to show different way

--- Factory.ts
+++ Factory.ts
@@ -38,3 +38,57 @@ toyotaFactory.buildCar({model:'Sienna', passengerCount: 8, driveTrain:'V6'})
 
 const hondaFactory = new HondaFactory('Honda')
 hondaFactory.buildCar({model:'Odyssey', passengerCount: 8, driveTrain:'V6'})
+
+
+
+
+
+
+// class Car{
+//     make: string;
+//     model: string;
+//     passengerCount: number;
+//     driveTrain: string
+// }
+
+// abstract class AutomobileFactory{
+//     car: Car;
+
+//     constructor(){}
+
+//    abstract buildCar(type: Car) : void;
+
+// }
+
+// class AutomobileCreator extends AutomobileFactory{
+    
+//     buildCar(type: Car){
+//         if(type.make == 'Toyota'){
+//             return new ToyotaFactory().buildCar(type)
+//         } else if (type.make == 'Honda'){
+//             return new HondaFactory().buildCar(type)
+//         } else{
+//             return 'Not found'
+//         }
+        
+//     }
+
+// }
+
+// class ToyotaFactory{
+    
+//     buildCar(type: Car){
+//         console.log(`${type.passengerCount} passenger ${type.make} ${type.model} with a ${type.driveTrain} engine was just made!`)
+//     }
+
+// }
+
+// class HondaFactory{
+
+//     buildCar(type: Car){
+//         console.log(`You just built a ${type.passengerCount} passenger ${type.make} ${type.model} with a ${type.driveTrain} engine.`)
+//     }
+// }
+
+// const toyotaMinivan = new AutomobileCreator().buildCar({make: 'Toyota', model:'Sienna', passengerCount: 8, driveTrain:'V6'})
+// const hondaMinivan = new AutomobileCreator().buildCar({make: 'Honda', model:'Odyssey', passengerCount: 8, driveTrain:'V6'})
